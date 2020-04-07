726e6f0734dfd80200006a0e043e5f5902be291b
Husnoro <Husnoro>
2020-04-07 13:19:18 +0500

    выписала код в Main + добавила курсы

--- Program.cs
+++ Program.cs
@@ -6,13 +6,34 @@ namespace dz0404
     {
         static void Main(string[] args)
         {
-            Converter currency = new Converter () { usd = 10.22, eur = 11.11, rub = 0.14};
+            Converter currency = new Converter () { usd = 10.22, eur = 11.11, rub = 0.141};
+            Console.Write(" Push *1* to change somoni to dollar \n Push *2* to change somoni to euro \n Push *3* to change somoni to rubli \n Your choise  ");
+            int choise = int.Parse(Console.ReadLine());
+            Console.Write("Enter amoung of somoni: ");
+            double somoni = double.Parse(Console.ReadLine());
+            switch (choise)
+            {
+                case 1: 
+                {   
+                    Console.WriteLine(currency.changeUsd(somoni) + " Somoni");
+                    Console.WriteLine(currency.changeToUsd(somoni)  + " Usd");
+                } break;
+                case 2: 
+                {   
+                    Console.WriteLine(currency.changeEur(somoni) + "  Somoni");
+                    Console.WriteLine(currency.changeToEur(somoni) + "  Euro");
+                } break;
+                case 3: 
+                {   
+                    Console.WriteLine(currency.changeRub(somoni) + " Somoni");
+                    Console.WriteLine(currency.changeToRub(somoni) + " Rub");
+                } break;   
+
+            }
 
         }
     }
-    /// <summary>
-    /// добавить соотношения
-    /// </summary>
+
     public class Converter 
     {
         public double usd { get; set; }
@@ -25,6 +46,30 @@ namespace dz0404
             this.rub = rub;
         }
         public Converter () {}
+        public double changeToUsd (double somoni)
+        {
+            return somoni/usd;
+        }
+        public double changeUsd ( double somoni )
+        {
+            return changeToUsd(somoni)*usd;
+        }
+        public double changeToEur (double somoni)
+        {
+            return somoni/eur;
+        }
+        public double changeEur ( double somoni )
+        {
+            return changeToEur(somoni)*eur;
+        }
+        public double changeToRub (double somoni)
+        {
+            return somoni/rub;
+        }
+        public double changeRub ( double somoni )
+        {
+            return changeToRub (somoni)*rub;
+        }
         
     }
 }
